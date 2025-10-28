fc0f8e9099491f1d3d3e47d64503a760254891d2
arvindkhoisnam <khoisnamarvind@gmail.com>
2025-10-28 12:21:05 +0530

    ws-client-fix

--- app/_comps/TVCandleModal.tsx
+++ app/_comps/TVCandleModal.tsx
@@ -1,14 +1,14 @@
 import { createPortal } from "react-dom";
 import TVCandleStick from "./TVCandleStick";
 import { cn } from "../lib/utils";
-import LIveStats from "./LIveStats";
+// import LIveStats from "./LIveStats";
 
 function TVCandleModal() {
   return createPortal(
     <div className="absolute top-0 left-0 h-full w-full p-2">
       <div className={cn("absolute inset-0 h-full w-full rounded-lg", "bg-neutral-900")}></div>
       <div className="absolute top-3 left-5 z-10 rounded-lg bg-neutral-900">
-        <LIveStats includeHeading={true} />
+        {/* <LIveStats includeHeading={true} /> */}
       </div>
       <TVCandleStick />
     </div>,
