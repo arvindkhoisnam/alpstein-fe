da9053cffdd77c0f9ebd0ec657a3349b2a4981d1
arvindkhoisnam <khoisnamarvind@gmail.com>
2025-11-20 22:45:36 +0530

    new-update

--- app/_comps/UserLogo.tsx
+++ app/_comps/UserLogo.tsx
@@ -1,34 +1,13 @@
 import Image from "next/image";
 import { useUser, useUserModal } from "../lib/zustand";
 import React from "react";
-// import axios, { AxiosError } from "axios";
-// import { redirect } from "next/navigation";
 
 function UserLogo() {
   const { currUser } = useUser();
   const { showUserModal, setShowUserModal } = useUserModal();
-  // const { toggleShowModal } = useShowSigninModal();
-
-  // useEffect(() => {
-  //   async function getData() {
-  //     try {
-  //       const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
-  //         withCredentials: true,
-  //       });
-  //       setUser(true, res.data.data);
-  //     } catch (err) {
-  //       if (err instanceof AxiosError) {
-  //         toggleShowModal(true);
-  //         redirect("/");
-  //       }
-  //     }
-  //   }
-  //   getData();
-  // }, [setUser, toggleShowModal]);
 
   return (
     <div className="flex items-center gap-1 md:gap-4">
-      {/* <DarkModelToggle /> */}
       <button
         className="cursor-pointer"
         onClick={() => {
